dff7797452c91f75c9af897467355b13c9de7a2c
Krajmark <kmark.mrkk@gmail.com>
2023-01-31 16:24:15 +0100

    initial: final project started

---
+++ src/main/java/com/greenfoxacademy/ebayclone/EBayCloneApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.ebayclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBayCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBayCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenfoxacademy/ebayclone/EBayCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.ebayclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBayCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
