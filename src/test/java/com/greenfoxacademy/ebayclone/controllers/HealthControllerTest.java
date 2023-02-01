6007ef2ed311d83f3f1259dca6eac277caa25df0
Krajmark <kmark.mrkk@gmail.com>
2023-02-01 17:17:09 +0100

    ci/test: added test covarage tool as dependency

---
+++ src/test/java/com/greenfoxacademy/ebayclone/controllers/HealthControllerTest.java
@@ -0,0 +1,33 @@
+package com.greenfoxacademy.ebayclone.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class HealthControllerTest {
+    //TODO: need security config for this?
+    /*@Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    void healthCheck() {
+
+    }*/
+}
