4d87dacc3da00fff3d6f70498797d59f0c399c8b
Krajmark <kmark.mrkk@gmail.com>
2023-02-02 20:10:46 +0100

    fix(ci): checkstyle

--- src/main/java/com/greenfoxacademy/ebayclone/security/SecurityConfig.java
+++ src/main/java/com/greenfoxacademy/ebayclone/security/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.greenfoxacademy.ebayclone.security;
 
-import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
