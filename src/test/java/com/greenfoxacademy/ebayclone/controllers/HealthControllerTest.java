aef948ae8153e0a408ca1f05335c060bb6453a7b
Krajmark <kmark.mrkk@gmail.com>
2023-02-02 18:09:03 +0100

    test: health check test

--- src/test/java/com/greenfoxacademy/ebayclone/controllers/HealthControllerTest.java
+++ src/test/java/com/greenfoxacademy/ebayclone/controllers/HealthControllerTest.java
@@ -1,33 +1,23 @@
 package com.greenfoxacademy.ebayclone.controllers;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 class HealthControllerTest {
-    //TODO: need security config for this?
-    /*@Autowired
+    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext context;
-
-    @BeforeEach
-    public void setup() {
-        this.mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .build();
-    }
-
     @Test
-    void healthCheck() {
-
-    }*/
+    void healthCheck() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/health/check")).andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
