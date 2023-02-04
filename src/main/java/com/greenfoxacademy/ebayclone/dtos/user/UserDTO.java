4819e3064c451f370dca429d48a9bfa8f57aef39
Krajmark <kmark.mrkk@gmail.com>
2023-02-04 18:15:30 +0100

    fix: @Min only works for numeric types -> use @Size

--- src/main/java/com/greenfoxacademy/ebayclone/dtos/user/UserDTO.java
+++ src/main/java/com/greenfoxacademy/ebayclone/dtos/user/UserDTO.java
@@ -3,13 +3,14 @@ package com.greenfoxacademy.ebayclone.dtos.user;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class UserDTO {
-    @Min(value = 6, message = "Username have to be longer than 5")
+    @Size(min = 6, message = "Username have to be longer than 5")
     @NotBlank(message = "Username cannot be blank")
     @NotNull(message = "Username cannot be null")
     private String username;
-    @Min(value = 6, message = "Password have to be longer than 5")
+    @Size(min = 6, message = "Password have to be longer than 5")
     @NotBlank(message = "Password cannot be blank")
     @NotNull(message = "Password cannot be null")
     private String password;
